467217b1adf4649046a06414520b033ce1ed8606
khasanjon <khasanjon.dev@gmail.com>
2023-12-10 11:38:46 +0500

    add some files

--- users/models.py
+++ users/models.py
@@ -1,3 +1,30 @@
-from django.db import models
+from django.db.models import Model, CharField, BigIntegerField, DateTimeField, IntegerField, ForeignKey, CASCADE
 
-# Create your models here.
+
+class User(Model):
+    first_name = CharField(max_length=150)
+    last_name = CharField(max_length=150)
+    telegram_id = BigIntegerField(unique=True)
+
+    created_at = DateTimeField(auto_now_add=True, blank=True)
+    updated_at = DateTimeField(auto_now=True, null=True, blank=True)
+
+
+class Science(Model):
+    name = CharField(max_length=150)
+    size = IntegerField()
+    keys = CharField(max_length=250)
+    # date
+    created_at = DateTimeField(auto_now_add=True, blank=True)
+    # relationship
+    author = ForeignKey(User, CASCADE, 'sciences')
+
+
+class Block(Model):
+    name = CharField(max_length=150, default='Blok Test')
+    size = IntegerField()
+    keys = CharField(max_length=250)
+    # date
+    created_at = DateTimeField(auto_now_add=True, blank=True)
+    # relationship
+    author = ForeignKey(User, CASCADE, 'blocks')
