fcc9b94cf5eabfde291228d36414a3110a83cba5
khasanjon <khasanjon.dev@gmail.com>
2023-12-10 11:52:16 +0500

    add some files

--- users/urls.py
+++ users/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
-from users.views import UserModelViewSet
+from users.views import UserModelViewSet, ScienceModelViewSet, BlockModelViewSet
 
 router = DefaultRouter()
 router.register('user', UserModelViewSet, 'user')
-
+router.register('science', ScienceModelViewSet, 'science')
+router.register('block', BlockModelViewSet, 'block')
 urlpatterns = [
     path('', include(router.urls))
 ]
