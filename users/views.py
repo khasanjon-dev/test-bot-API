7cbb4fb2e5b763e4564b34abdd75cf26868f80d9
khasanjon <khasanjon.dev@gmail.com>
2023-12-10 11:52:17 +0500

    add some files

--- users/views.py
+++ users/views.py
@@ -1,9 +1,19 @@
 from rest_framework.viewsets import ModelViewSet
 
-from users.models import User
-from users.serializers import UserModelSerializer
+from users.models import User, Science, Block
+from users.serializers import UserModelSerializer, ScienceModelSerializer, BlockModelSerializer
 
 
 class UserModelViewSet(ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserModelSerializer
+
+
+class ScienceModelViewSet(ModelViewSet):
+    queryset = Science.objects.all()
+    serializer_class = ScienceModelSerializer
+
+
+class BlockModelViewSet(ModelViewSet):
+    queryset = Block.objects.all()
+    serializer_class = BlockModelSerializer
