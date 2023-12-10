21f1f2aa111d8a5ef1b0365c273a60077037c7bd
khasanjon <khasanjon.dev@gmail.com>
2023-12-10 11:52:15 +0500

    add some files

--- users/serializers.py
+++ users/serializers.py
@@ -1,9 +1,21 @@
 from rest_framework.serializers import ModelSerializer
 
-from users.models import User
+from users.models import User, Science, Block
 
 
 class UserModelSerializer(ModelSerializer):
     class Meta:
         model = User
+        fields = '__all__'
+
+
+class ScienceModelSerializer(ModelSerializer):
+    class Meta:
+        model = Science
+        fields = '__all__'
+
+
+class BlockModelSerializer(ModelSerializer):
+    class Meta:
+        model = Block
         fields = '__all__'
