b61826eaa186765ab169fa94d7973803ecb2404a
khasanjon <khasanjon.dev@gmail.com>
2023-12-11 19:46:30 +0500

    add some files

--- users/models.py
+++ users/models.py
@@ -2,8 +2,8 @@ from django.db.models import Model, CharField, BigIntegerField, DateTimeField, I
 
 
 class User(Model):
-    first_name = CharField(max_length=150)
-    last_name = CharField(max_length=150)
+    first_name = CharField(max_length=150, null=True, blank=True)
+    last_name = CharField(max_length=150, null=True, blank=True)
     telegram_id = BigIntegerField(unique=True)
 
     created_at = DateTimeField(auto_now_add=True, blank=True)
