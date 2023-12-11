9f2de2035d5806a99959b10bd8f5f990ac25b42b
khasanjon <khasanjon.dev@gmail.com>
2023-12-11 19:46:32 +0500

    add some files

--- users/views.py
+++ users/views.py
@@ -1,15 +1,34 @@
-from rest_framework.viewsets import ModelViewSet
+from rest_framework.decorators import action
+from rest_framework.mixins import ListModelMixin, UpdateModelMixin, RetrieveModelMixin, CreateModelMixin, \
+    DestroyModelMixin
+from rest_framework.response import Response
+from rest_framework.viewsets import ModelViewSet, GenericViewSet
 
 from users.models import User, Science, Block
-from users.serializers import UserModelSerializer, ScienceModelSerializer, BlockModelSerializer
+from users.serializers import ScienceModelSerializer, BlockModelSerializer, UserSerializer
 
 
-class UserModelViewSet(ModelViewSet):
+class UserModelViewSet(RetrieveModelMixin, UpdateModelMixin, ListModelMixin, GenericViewSet):
     queryset = User.objects.all()
-    serializer_class = UserModelSerializer
+    serializer_class = UserSerializer
+
+    @action(['post'], False, 'get-or-create')
+    def get_or_create(self, request):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        telegram_id = serializer.data.get('telegram_id')
+        if User.objects.filter(telegram_id=telegram_id).exists():
+            User.objects.filter(telegram_id=telegram_id).update(**serializer.data)
+            user = User.objects.get(telegram_id=telegram_id)
+            serializer.data['id'] = user.id
+            return Response(serializer.data)
+        User.objects.create(**serializer.data)
+        user = User.objects.get(telegram_id=telegram_id)
+        serializer.data['id'] = user.id
+        return Response(serializer.data, 201)
 
 
-class ScienceModelViewSet(ModelViewSet):
+class ScienceModelViewSet(CreateModelMixin, RetrieveModelMixin, ListModelMixin, DestroyModelMixin, GenericViewSet):
     queryset = Science.objects.all()
     serializer_class = ScienceModelSerializer
 
